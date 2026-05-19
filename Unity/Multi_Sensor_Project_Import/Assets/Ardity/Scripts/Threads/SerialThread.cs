42186131f65c75cdeb7da0832bd4d2efcc7ab4c3
Johannes Kiel <59259239+johanneskiel@users.noreply.github.com>
2026-05-19 19:33:25 +0200

    Add files via upload

---
+++ Unity/Multi_Sensor_Project_Import/Assets/Ardity/Scripts/Threads/SerialThread.cs
@@ -0,0 +1,36 @@
+/**
+ * Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using UnityEngine;
+
+using System.IO.Ports;
+
+/**
+ * This class contains methods that must be run from inside a thread and others
+ * that must be invoked from Unity. Both types of methods are clearly marked in
+ * the code, although you, the final user of this library, don't need to even
+ * open this file unless you are introducing incompatibilities for upcoming
+ * versions.
+ * 
+ * For method comments, refer to the base class.
+ */
+public class SerialThread : SerialThreadLines
+{
+
+    public SerialThread(string portName,
+                        int baudRate,
+                        int delayBeforeReconnecting,
+                        int maxUnreadMessages,
+                        bool dropOldMessage,
+                        bool logDroppedMessages,
+                        bool dtrEnable,
+                        bool rtsEnable)
+        : base(portName, baudRate, delayBeforeReconnecting, maxUnreadMessages, dropOldMessage, logDroppedMessages, dtrEnable, rtsEnable)
+    {
+    }
+}
